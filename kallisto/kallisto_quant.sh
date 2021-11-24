0494b387049a5430eb249370b0c7d4c1a477da8e
hasnakhan <hasna.khan@mail.utoronto.ca>
2021-11-24 18:13:04 -0500

    update nov24

--- kallisto/kallisto_quant.sh
+++ kallisto/kallisto_quant.sh
@@ -2,5 +2,5 @@
 
 for file in /mnt/c/Users/hasna/Documents/MSc_project/20210920_rawseqdata/*.gz; do
 name=$(basename "$file" )
-kallisto quant -i AtRTD2_kallisto_index -o /mnt/c/Users/hasna/Documents/MSc_project/kallisto/kallisto/output/$name/ --single -l 200 -s 20 --genomebam --gtf AtRTD2_QUASI.gtf -t 3 -c chromosome_sizes.txt $file
+kallisto quant -i AtRTD2_kallisto_index_v3 -o /mnt/c/Users/hasna/Documents/MSc_project/kallisto/kallisto/output/$name/ --single -l 200 -s 20 --genomebam --gtf all_done_tidy.gtf -t 3 -c chromosome_sizes.txt $file
 done
